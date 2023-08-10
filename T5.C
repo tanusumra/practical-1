f0ee9d8ff33ea6b1a838420a69e9e71db0bca3e3
tanusumra <141604389+tanusumra@users.noreply.github.com>
2023-08-09 19:17:14 -0700

    Add files via upload

---
+++ T5.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,op;
+	clrscr();
+	printf("1.addition\n 2.substraction\n 3.multiplication\n");
+	printf("enter a & b::");
+	scanf("%d%d",&a&b);
+	switch (op);
+	{
+	case 1:
+		printf("sum of a & b::%d%d",a,b,a+b);
+		break;
+
+	getch();
+}
