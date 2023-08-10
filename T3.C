dc4a09257642f8237abc3d3c4ddc304dc8507979
tanusumra <141604389+tanusumra@users.noreply.github.com>
2023-08-09 19:15:11 -0700

    Add files via upload

---
+++ T3.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,sum=0;
+	clrscr();
+
+	printf("enter a::");
+	scanf("%d",&a);
+	printf("enter b::");
+	scanf("%d",&b);
+
+	sum=a+b;
+
+	printf("sum::%d",sum);
+
+	getch();
+}
